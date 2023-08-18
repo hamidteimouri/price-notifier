ad707671d57cfdcfe4b925e622d960cc02bb7bb7
Hamid Teimouri <iamhamidteimouri@gmail.com>
2023-08-18 15:33:46 +0330

    database migrations added

---
+++ src/migrations/20230818084432-create-item.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('items', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            base: {
+                type: Sequelize.STRING
+            },
+            quote: {
+                type: Sequelize.STRING
+            },
+            done: {
+                type: Sequelize.BOOLEAN
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updated_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('items');
+    }
+};
